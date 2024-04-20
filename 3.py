d0c8aef7be4e1fc62285406a707a11c5eeade1f7
daryazaitseva <zaytsevaart@gmail.com>
2024-04-20 13:03:40 +0700

    changes

--- 3.py
+++ 3.py
@@ -1,12 +1,7 @@
 n = int(input())
-if n % 2 == 0 and n >= 2:
-    print(2)
-else:
-    k = []
-    for i in range(3, n // 2 + 1):
-        if n % i == 0:
-            k.append(i)
-    if len(k) == 0:
-        print('невозможно')
-    else:
-        print(k[0])
+for i in range(2, n + 1):
+    if n % i == 0:
+        print(i)
+        break
+
+
